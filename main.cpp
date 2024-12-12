c916377c0930ef669f663d79bb845d51fdbb4532
s0chu <64419419+s0chu@users.noreply.github.com>
2024-12-12 17:08:34 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,17 +17,17 @@ const int scrollUnit = 10;
 const int timeUnit = 2000;
 const char cursorChar = ' ';
 const int N = 1e8 + 10;
-const int BUCKET = 2e4;
+const int BUCKET = 5e4;
 
 char input[N];
 float charHeight[C][C];
 int fontSize = 20;
+pair < int, int > segmOnScreen[C];
 
 int recalculateHeightLine()
 {
     return fontSize + 10;
 }
-
 
 int cursorHeight = 0, cursorWidth = 0;
 int navBarOffset = 50;
@@ -64,7 +64,7 @@ namespace String
             text.setCharacterSize(fnt);
             text.setString("Lorem Ipsum aaaaAAAAAppyPPPOOPOPOABCDFERFTDFDFAASDGgÊg|||");
             charHeight[fnt]['t'] = text.getGlobalBounds().height;
-            cerr << charHeight[fnt]['t'] << '\n';
+          //  cerr << charHeight[fnt]['t'] << '\n';
         }
     }
 
@@ -440,15 +440,18 @@ namespace String
         recalculate(T);
     }
 
-    string constructRenderedLine(int i, Treap*& T, int Xoffset)
+    string constructRenderedLine(int i, Treap*& T, int Xoffset , int I)
     {
         string txt = "";
         int p1 = String::findKthLine(i, T);
+        segmOnScreen[I] = { -1 , -1 };
         if (String::len(T) + 1 == p1 || String::get(p1, T) == 10) return txt;
         int p2 = String::findNextEndline(p1, T) - 1;
 
         int t1 = String::getFirstSeen(p1, p2, Xoffset, T);
         int t2 = String::getLastSeen(p1, p2, Xoffset + WIDTH - cntRowsOffset, T);
+
+        segmOnScreen[I] = { t1 , t2 };
 
         if (t1 == -1 || t2 == -1) return txt;
         String::traverseString(t1, t2, T, txt);
@@ -848,7 +851,7 @@ public:
     }
 };
 
-void splitCursorLine(sf::Text& text, sf::Text& h1, sf::Text& h2, string& txt, int posCursorOnScreen)
+void splitCursorLine(sf::Text& text, sf::Text& h1, sf::Text& h2, string& txt, int posCursorOnScreen , int fp)
 {
     if (txt.size() == 0)
     {
@@ -857,6 +860,12 @@ void splitCursorLine(sf::Text& text, sf::Text& h1, sf::Text& h2, string& txt, in
         return;
     }
 
+    if (fp == -1)
+    {
+        h1 = text;
+        h2.setString("");
+        return;
+    }
     string s1, s2;
 
     for (int i = 0; i < posCursorOnScreen - 1; i++) s1 += txt[i];
@@ -949,21 +958,156 @@ int main()
 
     sf::RectangleShape cursorBox;
     sf::RectangleShape cursorLineHighlight;
-    cursorLineHighlight.setFillColor(sf::Color(0, 0, 0, 50));
+    vector < sf::RectangleShape > selectedBoxes;
+    sf::RectangleShape box;
+    cursorLineHighlight.setFillColor(sf::Color(0, 0, 0, 30));
 
-    string path = "";
+    string path = "" , buffer = "";
     int Timer = 0;
     bool cursorOnScreen = 0;
+    bool cursorLineOnScreen = 1;
+    bool leftButtonPressed = 0;
+    bool selectFlag = 0;
+    bool ctrlX = 0, ctrlV = 0, ctrlC = 0;
+
+    int posCursorOnHold = -1;
+    pair < int, int > segmSelected;
+
     int lastDone = 0;
     int nr = 0;
+    int cntX = 0;
 
     while (window.isOpen())
     {
         bool flag = 0;
         bool fontChanged = 0;
         bool renderAgain = 0;
+       // bool ctrlX = 0 , ctrlY = 0 , ctrlV = 0;
+        
 
-        while (window.pollEvent(event))
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+            //sf::Vector2i globalPosition = sf::Mouse::getPosition();
+
+            if (localPosition.x >= cntRowsOffset && localPosition.y >= navBarOffset && localPosition.x < WIDTH && localPosition.y < HEIGHT)
+            {
+                int newPosCursor = moveCursorToClick(localPosition, S, scrollUnitY, l1, l2, Xoffset);
+                // cerr << "newpos: " << newPosCursor << '\n';
+
+                if (leftButtonPressed == 0)
+                {
+                    int posCursor = String::findCursorPosition(S);
+                    posCursorOnHold = newPosCursor;
+                    String::del(posCursor, S);
+                    leftButtonPressed = 1;
+                    selectFlag = 0;
+
+                    if (newPosCursor <= posCursor) String::insert(newPosCursor, S);
+                    else String::insert(newPosCursor - 1, S), --posCursorOnHold;
+                }
+                else if (newPosCursor != posCursorOnHold)
+                {
+                    selectFlag = 1;
+
+                    if (newPosCursor < posCursorOnHold) segmSelected = { newPosCursor , posCursorOnHold - 1 };
+                    else segmSelected = { posCursorOnHold + 1 , newPosCursor };
+                }
+
+
+                flag = 1;
+                renderAgain = 1;
+            }
+
+            if (localPosition.x < cntRowsOffset)
+            {
+                Xoffset -= scrollUnitX;
+                Xoffset = max(0, Xoffset);
+            }
+
+            if (localPosition.x >= WIDTH)
+            {
+                Xoffset += scrollUnitX;
+            }
+
+            if (localPosition.y < navBarOffset)
+            {
+                Yoffset -= scrollUnitY;
+                Yoffset = max(0, Yoffset);
+            }
+
+            if (localPosition.y >= HEIGHT)
+            {
+                Yoffset += scrollUnitY;
+            }
+
+            flag = 1;
+            renderAgain = 1;
+        }
+        else if (ctrlX == 0 && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+        {
+            ctrlX = 1;
+            int L = segmSelected.first;
+            int R = segmSelected.second;
+
+            String::Treap* s1 = 0, * s2 = 0, * s3 = 0;
+            String::split(S, s2, s3, R);
+            String::split(s2, s1, s2, L - 1);
+
+            buffer = String::constructString(s2);
+            //++cntX;
+
+            cerr << buffer << '\n';
+
+            delete s2;
+
+            String::merge(S, s1, s3);
+            selectFlag = 0;
+            flag = 1;
+            renderAgain = 1;
+
+            renderAgain |= updateViewX(S, Xoffset, scrollUnitX);
+            renderAgain |= updateViewY(S, Yoffset, scrollUnitY);
+        }
+        else if(ctrlC == 0 && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+        {
+            ctrlC = 1;
+            int L = segmSelected.first;
+            int R = segmSelected.second;
+
+            String::Treap* s1 = 0, * s2 = 0, * s3 = 0;
+            String::split(S, s2, s3, R);
+            String::split(s2, s1, s2, L - 1);
+
+            buffer = String::constructString(s2);
+
+            String::merge(S, s1, s2);
+            String::merge(S, S, s3);
+
+            flag = 1;
+            renderAgain = 1;
+        }
+        else if (ctrlV == 0 && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::V))
+        {
+            int posCursor = String::findCursorPosition(S);
+            ctrlV = 1;
+
+            cerr << "I am here" << '\n';
+
+            for (auto ch : buffer)
+            {
+                String::insert(posCursor, S , ch);
+                posCursor++;
+            }
+
+            flag = 1;
+            renderAgain = 1;
+            selectFlag = 0;
+
+            renderAgain |= updateViewX(S, Xoffset, scrollUnitX);
+            renderAgain |= updateViewY(S, Yoffset, scrollUnitY);
+        }
+        else while (window.pollEvent(event))
         {
             if (event.type == sf::Event::MouseMoved)
             {
@@ -982,6 +1126,38 @@ int main()
             }
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::MouseButtonReleased)
+            {
+                int key = event.key.code;
+
+                if (key == 0)
+                {
+                    leftButtonPressed = 0;
+                }
+            }
+
+            if (event.type == sf::Event::KeyReleased)
+            {
+                int key = event.key.code;
+
+                cerr << "this key " << key << '\n';
+
+                if (key == 23)
+                {
+                    ctrlX = 0;
+                }
+                else if (key == 2)
+                {
+                    ctrlC = 0;
+                }
+                else if (key == 21)
+                {
+                    ctrlV = 0;
+                }
+               
+                break;
+            }
 
             if (event.type == sf::Event::MouseButtonPressed)
             {
@@ -1123,22 +1299,12 @@ int main()
                 }
                 else if (key == 0) ///click random pe ecran ca sa schimbi unde e cursorul
                 {
-                    int newPosCursor = moveCursorToClick(localPosition, S, scrollUnitY, l1, l2, Xoffset);
-                   // cerr << "newpos: " << newPosCursor << '\n';
-                    int posCursor = String::findCursorPosition(S);
-                    String::del(posCursor, S);
-
-                    if (newPosCursor <= posCursor) String::insert(newPosCursor, S);
-                    else String::insert(newPosCursor - 1, S);
-
-                    flag = 1;
-                    renderAgain = 1;
-
                     break;
                 }
                 else break;
 
                 flag = 1;
+                selectFlag = 0;
 
                 renderAgain |= updateViewX(S, Xoffset, scrollUnitX);
                 renderAgain |= updateViewY(S, Yoffset, scrollUnitY);
@@ -1148,7 +1314,8 @@ int main()
 
             if (event.type == sf::Event::KeyPressed)
             {
-                cerr << event.key.code << '\n';
+                //selectFlag = 0;
+               // cerr << event.key.code << '\n';
                 int key = event.key.code;
 
                 if (key == 36) ///escape
@@ -1211,7 +1378,8 @@ int main()
                 else break;
 
                 flag = 1;
-
+                selectFlag = 0;
+               // cerr << "enter here" << '\n';
                 renderAgain |= updateViewX(S, Xoffset, scrollUnitX);
                 renderAgain |= updateViewY(S, Yoffset, scrollUnitY);
 
@@ -1221,8 +1389,9 @@ int main()
             if (event.type == sf::Event::TextEntered) ///ce scrie user-ul
             {
                 int ch = event.text.unicode;
+               // cerr << ch << '\n';
 
-                if (ch == 27)
+                if (ch == 27 || ch == 24 || ch == 3 || ch == 22)
                     break;
 
                 int posCursor = String::findCursorPosition(S);
@@ -1239,6 +1408,7 @@ int main()
                 }
 
                 flag = 1;
+                selectFlag = 0;
 
                 renderAgain |= updateViewX(S, Xoffset, scrollUnitX);
                 renderAgain |= updateViewY(S, Yoffset, scrollUnitY);
@@ -1250,8 +1420,8 @@ int main()
             {
                 int direction = event.mouseWheel.delta;
 
-                if (direction == +1) Yoffset -= scrollUnitY, Yoffset = max(0, Yoffset);
-                if (direction == -1) Yoffset += scrollUnitY;
+                if (direction == +1) Xoffset -= scrollUnitX, Xoffset = max(0, Xoffset);
+                if (direction == -1) Xoffset += scrollUnitX;
 
                 renderAgain = 1;
                 flag = 1;
@@ -1316,12 +1486,12 @@ int main()
                 l1 = max(1, (Yoffset - 1 - navBarOffset) / scrollUnitY + 1);
                 l2 = min(numberOfLines , max(1, (Yoffset - 1 + HEIGHT - navBarOffset) / scrollUnitY));
 
-                cerr << l1 << ' ' << l2 << '\n';
+                //cerr << l1 << ' ' << l2 << '\n';
 
                 sizeRLines = 0;
 
                 for (int i = l1; l1 > 0 && l2 > 0 && l1 <= l2 && i <= l2; i++)
-                    updateTextLine(sizeRLines, renderLines, String::constructRenderedLine(i, S, Xoffset));
+                    updateTextLine(sizeRLines, renderLines, String::constructRenderedLine(i, S, Xoffset , i - l1));
 
                 updateSmartRender(text, text1, text2, img1, img2, l1, l2, cursorLine, scrollUnitY);
             }
@@ -1329,28 +1499,69 @@ int main()
             {
                 if (cursorLine >= l1 && cursorLine <= l2)
                 {
-                    updateTextLine(cursorLine - l1, renderLines, String::constructRenderedLine(cursorLine, S, Xoffset));
+                    updateTextLine(cursorLine - l1, renderLines, String::constructRenderedLine(cursorLine, S, Xoffset , cursorLine - l1));
                     text.setString(renderLines[cursorLine - l1]);
                 }
             }
 
+            //cerr << "here" << ' ';
             string cursorTextLine = (cursorLine >= l1 && cursorLine <= l2 ? renderLines[cursorLine - l1] : "");
-            splitCursorLine(text, ptext1, ptext2, cursorTextLine, posCursor - fp + 1);
+            splitCursorLine(text, ptext1, ptext2, cursorTextLine, posCursor - fp + 1 , fp);
 
             if (cursorLine >= l1 && cursorLine <= l2)
+            {
+                cursorLineOnScreen = 1;
+            }
+            else cursorLineOnScreen = 0;
+
+            if (cursorLine >= l1 && cursorLine <= l2 && fp != -1)
             {
                 cursorBox.setSize(sf::Vector2f(cursorWidth, cursorHeight));
                 cursorBox.setPosition(ptext1.getGlobalBounds().width + cntRowsOffset + 4 + cursorWidth , text.getPosition().y);
 
                 cursorOnScreen = 1;
-                cursorLineHighlight.setSize(sf::Vector2f(WIDTH - cntRowsOffset , globalHeightLine));
-                cursorLineHighlight.setPosition(cntRowsOffset, text.getPosition().y - centerConst);
             }
             else cursorOnScreen = 0;
 
+            cursorLineHighlight.setSize(sf::Vector2f(WIDTH - cntRowsOffset, globalHeightLine));
+            cursorLineHighlight.setPosition(cntRowsOffset, text.getPosition().y - centerConst);
+
             lastCursorLine = cursorLine;
+
+            selectedBoxes.clear();
+
+            if (selectFlag)
+            {
+                int L = segmSelected.first;
+                int R = segmSelected.second;
+
+                for (int i = 0; i < sizeRLines; i++)
+                {
+                    int l = segmOnScreen[i].first;
+                    int r = segmOnScreen[i].second;
+
+                    int li = max(l, L);
+                    int ri = min(r, R);
+
+                    if (li > ri)
+                        continue;
+
+                    int y = i * globalHeightLine + navBarOffset;
+                    int x = cntRowsOffset;
+
+                    int w = String::findWidth(l, li - 1, S);
+                    int W = String::findWidth(li, ri, S);
+
+                    box.setPosition(w + cntRowsOffset , y);
+                    box.setSize(sf::Vector2f(W, globalHeightLine));
+                    box.setFillColor(sf::Color(0, 0, 0, 128));
+                    selectedBoxes.push_back(box);
+                }
+                
+            }
         }
 
+        if (selectFlag) for (auto box : selectedBoxes) window.draw(box);
         window.draw(img1);
         window.draw(ptext1);
         window.draw(ptext2);
@@ -1363,7 +1574,7 @@ int main()
         }
 
         if (cursorOnScreen) window.draw(cursorBox);
-        if (cursorOnScreen) window.draw(cursorLineHighlight);
+        if (cursorLineOnScreen && selectFlag == 0) window.draw(cursorLineHighlight);
         window.display();
     }
 
