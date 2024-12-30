9a4b3afe0f30961d52c749c53accf00020703788
Alex Croitoriu <alexcroitoriu2005@gmail.com>
2024-12-30 14:02:43 +0200

    improved generator

--- textGenerator.py
+++ textGenerator.py
@@ -2,7 +2,7 @@ input = (input("Enter desired file size (in MB): "))
 
 output = open("output.txt", "w")
 
-line = "Lorem ipsum dolor sit amet\n"
+line = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lorem ipsum, auctor sit amet ipsum eget, laoreet consectetur velit. Aliquam \nsit amet tristique nibh. Donec malesuada mauris orci, facilisis fringilla nulla molestie ac. Integer ultricies feugiat nunc vitae blandit. Proin \nfacilisis bibendum erat, nec blandit dui auctor at. Proin dui purus, volutpat pulvinar ultrices id, semper at odio. Nulla congue auctor urna \nsit amet gravida. Integer porta nisi vitae ultricies congue. Nunc pharetra tempus sem non commodo. Fusce vitae egestas lacus. Cras \npretium vel metus sit amet euismod. Sed sem elit, egestas sed elementum at, scelerisque nec ex.\n"
 lineCount = int(input) * 1000000 / len(line)
 
 for i in range(0, int(lineCount)):
