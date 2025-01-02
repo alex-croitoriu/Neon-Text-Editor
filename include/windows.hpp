ab137164f396a1971da352088df35277dc6b6572
Alex Croitoriu <alexcroitoriu2005@gmail.com>
2025-01-03 00:36:25 +0200

    Update windows.hpp

--- include/windows.hpp
+++ include/windows.hpp
@@ -4,7 +4,6 @@
 
 namespace Windows
 {
-    std::string getPathFromUser(std::string name);
     std::string getStringFromUser(std::string name);
     void throwMessage(std::string message);
     std::string saveAS();
